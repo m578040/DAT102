7ccdcb3e037ea372dcf6261df1d40f5f24cd1403
Morten Tordal <morten.tordal@hotmail.no>
2019-04-09 19:00:56 +0200

    2a ferdig

--- BSTre/src/no/hvl/dat102/Hjelpemetoder.java
+++ BSTre/src/no/hvl/dat102/Hjelpemetoder.java
@@ -2,10 +2,10 @@ package no.hvl.dat102;
 
 public class Hjelpemetoder {
 	public int finnMinHoyde(int antallNoder) {
-		
+		return 2;
 	}
 	
 	public int finnMaksHoyde(int antallNoder) {
-		
+		return 4;
 	}
 }
